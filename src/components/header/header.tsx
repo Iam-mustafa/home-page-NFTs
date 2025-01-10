2da8f7c13dd47fc21b92183948055104f22106d8
Muhammad Mustafa <mustafa-naveed@hotmail.com>
2025-01-10 23:43:19 +0500

    fix(build): header button width fix

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -69,13 +69,13 @@ export function Header() {
           </nav>
 
           {/* Sign Up Button */}
-          <button className="flex justify-center items-center space-x-3 w-[18%] self-stretch bg-gradient-to-r from-[#A259FF] to-[#377DF7] hover:from-transparent hover:to-transparent border-[#A259FF] border-2 text-white px-3 py-2 md:px-8 md:py-3 rounded-full transition-colors">
+          <button className="flex justify-center items-center space-x-3 self-stretch bg-gradient-to-r from-[#A259FF] to-[#377DF7] hover:from-transparent hover:to-transparent border-[#A259FF] border-2 text-white px-3 py-2 md:px-8 md:py-3 rounded-full transition-colors">
             <Gamepad2 className="w-5 h-5" />
             <span>Player Login</span>
           </button>
-          <button className="flex justify-center items-center space-x-2 w-[18%] self-stretch bg-gradient-to-r from-[#A259FF] to-[#377DF7] hover:from-transparent hover:to-transparent border-[#A259FF] border-2 text-white px-3 py-2 md:px-8 md:py-3 rounded-full transition-colors">
+          <button className="flex justify-center items-center space-x-2 self-stretch bg-gradient-to-r from-[#A259FF] to-[#377DF7] hover:from-transparent hover:to-transparent border-[#A259FF] border-2 text-white px-3 py-2 md:px-8 md:py-3 rounded-full transition-colors">
             <CodeSquare className="w-5 h-5" />
-            <span className="whitespace-nowrap">Developer Login</span>
+            <span>Developer Login</span>
           </button>
         </div>
       </div>
