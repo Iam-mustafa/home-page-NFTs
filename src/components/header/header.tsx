81ecda398bef7111a6bf6e98becfac5693addae5
Muhammad Mustafa <mustafa-naveed@hotmail.com>
2025-01-10 23:38:59 +0500

    fix(build): imports removed

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -75,7 +75,7 @@ export function Header() {
           </button>
           <button className="flex justify-center items-center space-x-2 w-[18%] self-stretch bg-gradient-to-r from-[#A259FF] to-[#377DF7] hover:from-transparent hover:to-transparent border-[#A259FF] border-2 text-white px-3 py-2 md:px-8 md:py-3 rounded-full transition-colors">
             <CodeSquare className="w-5 h-5" />
-            <span>Developer Login</span>
+            <span className="whitespace-nowrap">Developer Login</span>
           </button>
         </div>
       </div>
